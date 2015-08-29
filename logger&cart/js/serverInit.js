34d5eb7de510a8c3fb6af74b5cde279b171d81f9
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-08-29 18:30:07 +0300

    app structure, logger form, cart form added.

---
+++ logger&cart/js/serverInit.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Goran on 8/27/2015.
+ */
+
+Parse.initialize("MamLZB0oJXKh0gwOG2mOorfM9SwWhhLc4LxyqAhv", "lj9oAHVgugUF7ITMmr4J9OiHcX74k4T4p0CGqdZP");
+
---
+++ logger&cart/model/cart.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Goran on 8/28/2015.
+ */
+var Cart = Parse.Object.extend("Cart", {
+    cartProducts:[],
+    totalPrice: function () {
+        var sum = 0;
+        var prod = this.get("cartProducts");
+        foreach(p in prod)
+        {
+            sum += p.get("price");
+        }
+        return sum;
+    },
+    addProduct:function(product){
+        this.cartProducts.push(product);
+        return this;
+    },
+    removeProduct:function(product){
+     var index = this.cartProducts.indexOf(product);
+        if (index > -1) {
+            this.cartProducts.splice(index, 1);
+        }
+    },
+    uploadProducts:function(productList){
+        var promise = new Parse.Promise();
+        for(var i= 0,j=this.cartProducts.length;i<j;i+=1)
+        {
+            this.cartProducts[i].save();
+        }
+        setTimeout(function(){
+           promise.resolve(this);
+        },3000);
+       return promise;
+    }
+
+});
+
+function createCart() {
+    return new Cart();
+}
+
