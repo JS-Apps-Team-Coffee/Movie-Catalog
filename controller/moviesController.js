4650fb33328b1ce7ee89c4c38fc0967a67a8ab5f
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-08-30 16:11:49 +0300

    Facebook Like button added for each movie!

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -18,6 +18,11 @@ $(document).ready(function() {
                 $("#top-movie1").find("p[data-info='rating']").html(top4[0].rating);
                 $("#top-movie1").find("a[data-info='link']").attr("href", (top4[0].link));
 
+                //facebook
+                $("#top-movie1").find("a[data-info='fb-like']").attr("name", (top4[0].title));
+                $("#top-movie1").find("div[data-info='fb-like']").attr("data-href", ("http://moviecatalog.net84.net/#"+top4[0].title));
+
+
                 $("#top-movie1").find("img").attr('src', (top4[0].image));
 
                 $("#top-movie2").load('view/movieTemplate.html', function(data) {
@@ -31,6 +36,10 @@ $(document).ready(function() {
                     $("#top-movie2").find("a[data-info='link']").attr("href", (top4[1].link));
                     $("#top-movie2").find("img").attr('src', (top4[1].image));
 
+                    //fb
+                    $("#top-movie2").find("a[data-info='fb-like']").attr("name", (top4[1].title));
+                    $("#top-movie2").find("div[data-info='fb-like']").attr("data-href", ("http://moviecatalog.net84.net/#"+top4[1].title));
+
                     $("#top-movie3").load('view/movieTemplate.html', function(data) {
                         console.log($("#top-movie3"));
                         // debugger;
@@ -41,6 +50,9 @@ $(document).ready(function() {
                         $("#top-movie3").find("p[data-info='rating']").html(top4[2].rating);
                         $("#top-movie3").find("a[data-info='link']").attr("href", (top4[2].link));
                         $("#top-movie3").find("img").attr('src', (top4[2].image));
+                        //fb
+                        $("#top-movie3").find("a[data-info='fb-like']").attr("name", (top4[2].title));
+                        $("#top-movie3").find("div[data-info='fb-like']").attr("data-href", ("http://moviecatalog.net84.net/#"+top4[2].title));
 
                         $("#top-movie4").load('view/movieTemplate.html', function(data) {
                             console.log($("#top-movie3"));
@@ -52,6 +64,18 @@ $(document).ready(function() {
                             $("#top-movie4").find("p[data-info='rating']").html(top4[3].rating);
                             $("#top-movie4").find("a[data-info='link']").attr("href", (top4[3].link));
                             $("#top-movie4").find("img").attr('src', (top4[3].image));
+                            //fb
+                            $("#top-movie4").find("a[data-info='fb-like']").attr("name", (top4[3].title));
+                            $("#top-movie4").find("div[data-info='fb-like']").attr("data-href", ("http://moviecatalog.net84.net/#"+top4[3].title));
+
+
+                            (function(d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) return;
+                                js = d.createElement(s); js.id = id;
+                                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
                         })
                     })
                 })
