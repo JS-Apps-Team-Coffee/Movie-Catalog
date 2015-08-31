231f5dc94f5adb5febfaf414d81385e3ca47b10e
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-08-31 19:22:53 +0300

    Top movie template separate in .js file!

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -1,18 +1,32 @@
- requirejs(["../model/movie.js"], function(util) {
-    
-     $(document).ready(function() {
-         var top4 = [];
-         getTop4Movies().then(function(movies) {
-             top4 = movies;
-             $('#wrapper').load('view/firstPageTemplate.html', function() {
-                 for (var i = 1; i <= 4; i += 1) {
-                     var $movieWrapper = $('#top-movie' + i);
-                     var movieTemplateHtml = $('#movie-template').html();
-                     var template = Handlebars.compile(movieTemplateHtml);
+requirejs(["../model/movie.js"], function (util) {
+    requirejs(["../view/topMovieTemplate.js"], function (util) {
 
-                     $movieWrapper.html(template(top4[i - 1]));
-                 }
-             });
-         });
-     });
- });
+        $(document).ready(function () {
+            var top4 = [];
+            getTop4Movies().then(function (movies) {
+                top4 = movies;
+
+                var movieTemplateHtml = getTopMovieTemp();
+                var template = Handlebars.compile(movieTemplateHtml);
+
+                $('#wrapper').load('view/firstPageTemplate.html', function () {
+                    for (var i = 1; i <= 4; i += 1) {
+                        var $movieWrapper = $('#top-movie' + i);
+                        // var movieTemplateHtml = $('#movie-template').html();
+
+
+                        $movieWrapper.html(template(top4[i - 1]));
+                    }
+                });
+                (function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            });
+        });
+    });
+});
